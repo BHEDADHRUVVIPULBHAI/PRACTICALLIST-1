9c4fbd7fcba74ea4381d13a6da2540c863b26923
BHEDADHRUVVIPULBHAI <142769002+BHEDADHRUVVIPULBHAI@users.noreply.github.com>
2023-08-24 00:29:56 +0530

    Create pro_12.cpp

---
+++ list1/pro_12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n,i;
+	
+	cout<<"I am dhruv bheda "<<endl;
+	cout<<"En_no    220130318012 "<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"Enter the number : ";
+	cin>>n;
+	
+	for(i=1;i<10;i++)
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+	}
+	cout<<endl;
+}
