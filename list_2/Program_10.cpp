42df35a249a73f803d80f8819e05ba94d1fb8088
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-15 19:01:39 +0530

    Create Program_10.cpp

---
+++ list_2/Program_10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int print(int &p,int &q);
+int main()
+{
+	int p,q;
+	cout<<"Bheda Dhruv"<<endl;
+	cout<<"220130318012"<<endl; 
+	cout<<"Enter The Number P::";
+	cin>>p;
+	cout<<"Enter The Number Q::";
+	cin>>q;
+	
+	print(p,q);
+	return 0;
+}
+int print(int &p,int &q)
+{
+	cout<<"The Number's Multiplication is ::"<<p*q;
+}
