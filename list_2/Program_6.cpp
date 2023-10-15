f2bee311e21008221c021d658fe4a1b64e0b3558
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-15 19:04:48 +0530

    Update Program_6.cpp

--- list_2/Program_6.cpp
+++ list_2/Program_6.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	// function setfill 
 	int a =10;
-	cout<<"I am Prince Solanki   "<<endl;
-	cout<<"En_no    220130318032 "<<endl;
+	cout<<"I am Bheda Dhruv   "<<endl;
+	cout<<"En_no    220130318012 "<<endl;
 	cout<<"--------------------- "<<endl;
 	cout<<"The value of A :   "<<setw(5)<<setfill('*')<<a;
 	return 0;
