c4be6ad6c7a3bacdb6fbd228da50d50a9ca533d4
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-21 17:56:45 +0530

    Create Pro_11.cpp

---
+++ list_2/Pro_11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int add;
+int &sum(int a,int b);
+int main()
+{
+    int a,b,answer;
+	cout<<"Dhruv Bheda "<<endl;
+	cout<<"220130318012"<<endl;
+
+    cout<<"Enter The A ::";
+    cin>>a;
+    cout<<"Enter The B ::";
+    cin>>b;
+    answer= sum(a,b);
+    cout << "Sum of " <<a<<" + "<<b<<" is :: "<< answer <<endl;
+}
+int &sum(int a,int b){
+    add=a+b;
+    return add;
+}
