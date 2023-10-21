c3736b334032de586c0067bcea5d88280efa16b3
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-21 18:00:49 +0530

    Create Program_15.cpp

---
+++ list_2/Program_15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+inline int Square(int a) 
+{
+	return a*a;
+}
+int main() 
+{
+	int a;
+	cout<<" Dhruv Bheda "<<endl;
+	cout<<"220130318012"<<endl;
+	cout<<"Enter The Value ::"<<endl;
+	cin>>a;
+
+	cout<<"The Square Of The Number is ::"<<Square(a);
+
+	return 0;
+}
