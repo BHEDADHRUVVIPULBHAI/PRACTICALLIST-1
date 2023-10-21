61dcddede773dd8bd7f48387396acd9a62735218
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-21 18:06:18 +0530

    Create Program_2.cpp

---
+++ list_3/Program_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct student
+{
+	char a;
+	float c;
+	char b;
+};
+int main()
+{
+	cout<<"Dhruv Bheda"<<endl;
+	cout<<"220130318012"<<endl;
+	cout<<"-------------"<<endl;
+	cout<<"sizeof(student) is ::"<<sizeof(student);
+	return 0;
+}
