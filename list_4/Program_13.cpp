580a87121852821c5bcb47097d40443d2ccdbe7d
DHRUVBHEDA <142769002+DHRUVBHEDA@users.noreply.github.com>
2023-10-22 17:36:54 +0530

    Create Program_13.cpp

---
+++ list_4/Program_13.cpp
@@ -0,0 +1,26 @@
+//Write a C++ program to demonstrate use of Destructor. 
+#include<iostream>
+using namespace std;
+class rectangle
+{
+	int l,b;
+	public:
+		rectangle()
+		{
+			cout<<"Enter length of Rectangle ::";
+			cin>>l;
+			cout<<"Enter Bredth of Rectangle ::";
+			cin>>b;
+			
+			cout<<"Area of Rectangle is ::"<<l*b;
+		}
+		
+		~rectangle(){}
+		
+};
+int main()
+{
+	cout<<"Dhruv Bheda "<<endl;
+	cout<<"220130318012"<<endl;
+	rectangle r1;
+}
